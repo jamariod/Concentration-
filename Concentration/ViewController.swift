d70a8617854d87a87df916c083eeae25ee1e9005
jamariod <j25ghs99@aol.com>
2020-05-24 12:35:07 -0400

    Added  and connected four cards and flip label to the UI.

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var flipCard = 0 {
+        didSet {
+            flipCardLabel.text = "Flips: \(flipCard)"
+        }
     }
 
+    @IBOutlet weak var flipCardLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["🎃", "👻", "🎃", "👻"]
 
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+           flipCard(withEmoji: emojiChoices[cardNumber], on: sender) 
+        } else {
+            print("chosen card was not in cardButtons")
+        }
+    }
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
 }
 
